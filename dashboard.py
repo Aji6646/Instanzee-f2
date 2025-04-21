ccb2adfd98af2cb060d4b2ffb34e7590c5fb8103
krztech <krztechdb@gmail.com>
2025-04-21 22:58:19 +0530

    fixed hugging face issue in dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -20,17 +20,6 @@ import instaloader
 from instaloader import Post
 import re  
 
-# AutoTokenizer.from_pretrained("twitter-roberta-base-sentiment", cache_dir="./model")
-# AutoModelForSequenceClassification.from_pretrained("twitter-roberta-base-sentiment", cache_dir="./model")
-
-
-# from huggingface_hub import snapshot_download
-
-# snapshot_download(
-#     repo_id="cardiffnlp/twitter-roberta-base-sentiment",
-#     local_dir="./models/twitter-roberta-base-sentiment",
-#     local_dir_use_symlinks=False  # ensures full files are copied
-# )
 
 # -------------------------------
 # Session State and Access Control
